47c5732874ccdf61afae1e600bacaf39f9025894
sanyalu <sanyalu97@gmail.com>
2022-08-30 22:01:53 +0200

    Refactor OnOff

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -7,14 +7,14 @@ type OnOffPropsType = {
 const OnOff = ({}:OnOffPropsType) => {
     const [isOn, setIsOn] = useState<boolean>(false);
 
-    const changeIsOn = () => {
-        setIsOn(!isOn);
+    const changeIsOn = (value: boolean) => {
+        setIsOn(value);
     }
 
     return (
         <div className='on-off'>
-            <div onClick={changeIsOn} className={'on-off__left' + (isOn ? ' active' : '')}>On</div>
-            <div onClick={changeIsOn} className={'on-off__right' + (!isOn ? ' active' : '')}>Off</div>
+            <div onClick={() => changeIsOn(true)} className={'on-off__left' + (isOn ? ' active' : '')}>On</div>
+            <div onClick={() => changeIsOn(false)} className={'on-off__right' + (!isOn ? ' active' : '')}>Off</div>
             <div className={"indicator" + (isOn ? ' active' : '')}></div>
         </div>
     );
