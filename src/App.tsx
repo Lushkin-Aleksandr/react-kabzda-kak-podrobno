7874be02a918a5952c0e6fe78f03c936fa3ae9ed
sanyalu <sanyalu97@gmail.com>
2022-09-06 16:08:43 +0200

    app for controlled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React, {useState} from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValuesType} from "./components/Rating/Rating";
 import OnOff from "./components/OnOff/OnOff";
 import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
+import ControlledOnOff from "./components/ControlledOnOFF/ControlledOnOff";
 
 function App() {
 
-
+    const [ratingValue, setRatingValue] = useState<RatingValuesType>(0);
+    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true);
+    const [isOn, setIsOn] = useState<boolean>(true)
 
     return (
         <div className="App">
             <PageTitle title='QWERTY'/>
             <OnOff />
+            <ControlledOnOff isOn={isOn} changeIsOn={value => setIsOn(value)}/>
             <UncontrolledAccordion title={'Uncontrolled 1'} />
             <UncontrolledAccordion title={'Uncontrolled 2'} />
             <UncontrolledRating/>
-            {/*<Rating value={1}/>*/}
-            {/*<Accordion title='Menu' collapsed={false}/>*/}
+            <Rating value={ratingValue} changeValue={(value) => setRatingValue(value)}/>
+            <Accordion title='Menu' collapsed={accordionCollapsed} changeCollapsed={() => setAccordionCollapsed(!accordionCollapsed)}/>
             {/*<Accordion title='Users' collapsed={true}/>*/}
 
 
