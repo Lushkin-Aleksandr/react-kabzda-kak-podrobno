4481a429728202c67739baa180eb42e6a1aca33f
sanyalu <sanyalu97@gmail.com>
2022-09-20 22:40:51 +0200

    refactor accordion

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,11 +1,22 @@
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import {useState} from "react";
 import Accordion from "./Accordion";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Accordion',
     component: Accordion,
-    argTypes: { changeCollapsed: {action: 'clicked to change collapsed'}}
+    args: {
+        items: [
+            {title: 'First', value: '1'},
+            {title: 'Second', value: '2'},
+            {title: 'Third', value: '3'},
+            {title: 'Fourth', value: '4'},
+        ],
+        changeCollapsed: () => action('clicked to change collapsed'),
+        itemClickCallback: action(`clicked to item.`)
+    }
+
 } as ComponentMeta<typeof Accordion>
 
 const Template: ComponentStory<typeof Accordion> = (args) => <Accordion {...args}/>
@@ -13,4 +24,25 @@ export const CollapsedAccordion = Template.bind({})
 CollapsedAccordion.args = {title: 'Collapsed Accordion', collapsed: true}
 
 export const NonCollapsedAccordion = Template.bind({})
-NonCollapsedAccordion.args = {title: 'Non Collapsed Accordion', collapsed: false}
+NonCollapsedAccordion.args = {
+    title: 'Non Collapsed Accordion',
+    collapsed: false,
+}
+
+export const AccordionWithClickableItems = () => {
+    const [collapsed, setCollapsed] = useState(false)
+
+    return (
+        <Accordion
+            items={[
+                {title: 'First', value: '1'},
+                {title: 'Second', value: '2'},
+                {title: 'Third', value: '3'},
+                {title: 'Fourth', value: '4'},
+            ]}
+            collapsed={collapsed}
+            title={'AccordionWithClickableItems'}
+            changeCollapsed={() => setCollapsed(!collapsed)}
+            itemClickCallback={(value: string) => alert(value)}/>
+    )
+}
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {MouseEvent} from "react";
+
+export type AccordionItemType = {
+    title: string
+    value: string
+}
 
 type AccordionPropsType = {
     title: string,
     collapsed?: boolean
     changeCollapsed: () => void
+    items: AccordionItemType[]
+    itemClickCallback: (value: string) => void
 }
 
 function Accordion(props: AccordionPropsType) {
@@ -11,7 +18,7 @@ function Accordion(props: AccordionPropsType) {
     return (
         <div>
             <AccordionTitle onClick={props.changeCollapsed} title={props.title}/>
-            {!props.collapsed && <AccordionBody/>}
+            {!props.collapsed && <AccordionBody itemClickCallback={props.itemClickCallback} items={props.items}/>}
         </div>
     )
 }
@@ -28,16 +35,23 @@ function AccordionTitle(props: AccordionTitlePropsType) {
     )
 }
 
-type AccordionBodyPropsType = {}
+type AccordionBodyPropsType = {
+    items: AccordionItemType[]
+    itemClickCallback: (value: string) => void
+}
 
 function AccordionBody(props: AccordionBodyPropsType) {
 
     return (
         <div>
             <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
+                {props.items.map((item, index) => {
+                    const onClick = () => {
+                        props.itemClickCallback(item.value);
+                    }
+
+                    return <li key={index} onClick={onClick}>{item.title}</li>
+                })}
             </ul>
         </div>
     )
