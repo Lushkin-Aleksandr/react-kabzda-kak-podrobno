86a5140580b71abbb2993d36217f045396806b20
sanyalu <sanyalu97@gmail.com>
2022-09-20 15:12:52 +0200

    controlled: input, checkbox and select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -2,8 +2,7 @@ import {ComponentMeta, ComponentStory} from "@storybook/react";
 import {ChangeEvent, useRef, useState} from "react";
 
 export default {
-    title: 'UncontrolledInput'
-
+    title: 'Input'
 }
 
 
@@ -37,7 +36,63 @@ export const UncontrolledInputWithButton = () => {
         <>
             <input
                 ref={inputRef}/>
-            <button onClick={onClick}>Get value</button> - {value}
+            <button onClick={onClick}>Get value</button>
+            - {value}
         </>
     )
 }
+
+export const ControlledInput = () => {
+    const [inputValue, setInputValue] = useState<string>('');
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.currentTarget.value);
+    }
+
+    return (
+        <>
+            <input
+                value={inputValue}
+                onChange={onChange}/> - {inputValue}
+        </>
+    )
+}
+
+export const ControlledCheckbox = () => {
+    const [checkboxValue, setCheckboxValue] = useState<boolean>(false);
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCheckboxValue(e.currentTarget.checked);
+    }
+
+    return (
+        <>
+            <input
+                type={"checkbox"}
+                checked={checkboxValue}
+                onChange={onChange}/> - checked: {checkboxValue.toString()}
+        </>
+    )
+}
+
+export const ControlledSelect = () => {
+    const [selectValue, setSelectValue] = useState<string>('none');
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSelectValue(e.currentTarget.value);
+    }
+
+    return (
+        <>
+            <select
+                value={selectValue.toString()}
+                onChange={onChange}>
+                <option value='none'>none</option>
+                <option value="1">Moscow</option>
+                <option value="2">Munich</option>
+                <option value="3">Rome</option>
+            </select> - {selectValue}
+        </>
+    )
+}
+
