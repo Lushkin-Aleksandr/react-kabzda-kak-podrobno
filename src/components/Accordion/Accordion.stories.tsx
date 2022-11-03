7368b5081c5047324c0d1a1eeb3a85ac4639aea0
sanyalu <sanyalu97@gmail.com>
2022-11-03 12:57:48 +0100

    refactor Accordion story

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,48 +1,50 @@
 import {ComponentMeta, ComponentStory} from "@storybook/react";
-import {useState} from "react";
 import Accordion from "./Accordion";
 import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Accordion',
     component: Accordion,
+    argTypes: {
+        items: {
+            description: 'Elements of Accordion'
+        },
+        changeCollapsed: {
+            description: 'Calling after click on title'
+        },
+        collapsed: {
+            description: 'Collapse status'
+        },
+        itemClickCallback: {
+            description: 'Calling after click on Accordion item'
+        }
+    },
     args: {
+        title: 'Accordion',
         items: [
             {title: 'First', value: '1'},
             {title: 'Second', value: '2'},
             {title: 'Third', value: '3'},
             {title: 'Fourth', value: '4'},
         ],
-        changeCollapsed: () => action('clicked to change collapsed'),
+        changeCollapsed: action('clicked to change collapsed'),
         itemClickCallback: action(`clicked to item.`)
-    }
+    },
+
 
 } as ComponentMeta<typeof Accordion>
 
 const Template: ComponentStory<typeof Accordion> = (args) => <Accordion {...args}/>
-export const CollapsedAccordion = Template.bind({})
-CollapsedAccordion.args = {title: 'Collapsed Accordion', collapsed: true}
 
-export const NonCollapsedAccordion = Template.bind({})
-NonCollapsedAccordion.args = {
+export const Collapsed = Template.bind({})
+Collapsed.args = {title: 'Collapsed Accordion', collapsed: true}
+
+export const NonCollapsed = Template.bind({})
+NonCollapsed.args = {
     title: 'Non Collapsed Accordion',
     collapsed: false,
 }
 
-export const AccordionWithClickableItems = () => {
-    const [collapsed, setCollapsed] = useState(false)
 
-    return (
-        <Accordion
-            items={[
-                {title: 'First', value: '1'},
-                {title: 'Second', value: '2'},
-                {title: 'Third', value: '3'},
-                {title: 'Fourth', value: '4'},
-            ]}
-            collapsed={collapsed}
-            title={'AccordionWithClickableItems'}
-            changeCollapsed={() => setCollapsed(!collapsed)}
-            itemClickCallback={(value: string) => alert(value)}/>
-    )
-}
+
+
